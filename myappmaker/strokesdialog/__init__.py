10b98bda5c0e2439a32c49cdc75211e0050181eb
Kennedy Richard <kennedy@kennedyrichard.com>
2025-02-06 10:18:11 -0300

    misc: improvements on stroke settings dialog
    Refactored a bit and added a new "Set/reset strokes" buttons.
    
    The underlying functionality of the buttons isn't implemented yet.

--- myappmaker/strokesdialog/__init__.py
+++ myappmaker/strokesdialog/__init__.py
@@ -1,4 +1,6 @@
 
+### standard library import
+from functools import partial
 
 ### third-party imports
 
@@ -9,6 +11,7 @@ from PySide6.QtWidgets import (
     QWidget,
     QLabel,
     QCheckBox,
+    QPushButton,
 )
 
 from PySide6.QtSvgWidgets import QSvgWidget
@@ -27,6 +30,24 @@ from .getnotfoundsvg import get_not_found_icon_svg_text
 ### constant
 STROKE_SIZE = QSize(300, 300)
 
+### unmarked checkbox
+
+def get_check_box(checked=True):
+
+    check_box = QCheckBox()
+    check_box.setCheckState(
+        getattr(
+            Qt.CheckState,
+            'Checked' if checked else 'Unchecked',
+        )
+    )
+    check_box.setEnabled(False)
+
+    return check_box
+
+get_checked_check_box = partial(get_check_box, True)
+get_unchecked_check_box = partial(get_check_box, False)
+
 
 ### dialog definition
 
@@ -36,57 +57,85 @@ class StrokeSettingsDialog(QDialog):
 
         super().__init__(parent)
 
+        self.setWindowTitle('Stroke settings')
+
         grid = self.grid = QGridLayout()
 
-        ### label
+        ### define captions
 
-        label_strokes_dir = STROKES_DATA_DIR / 'label_strokes_dir'
+        for col, label_text in enumerate(
+            (
+                "Widget name",
+                "Strokes",
+                "Set/reset",
+            )
+        ):
 
-        grid.addWidget(QLabel('Label'), 0, 0)
-        grid.addWidget(StrokesDisplay(label_strokes_dir), 0, 1)
+            label = QLabel(label_text)
+            label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+            grid.addWidget(label, 0, col)
 
-        ### unmarked checkbutton
+        ### define contents
 
-        unmarked_checkbutton_strokes_dir = (
-            STROKES_DATA_DIR / 'unmarked_checkbutton_strokes_dir'
-        )
+        for row, (widget_key, get_widget) in enumerate(
 
-        unchecked_checkbox = QCheckBox()
-        unchecked_checkbox.setCheckState(Qt.CheckState.Unchecked)
+            (
+                ('label', partial(QLabel, 'A label')),
+                ('unchecked_check_box', get_unchecked_check_box),
+                ('checked_check_box', get_checked_check_box),
+            ),
 
-        grid.addWidget(unchecked_checkbox, 1, 0)
-        grid.addWidget(StrokesDisplay(unmarked_checkbutton_strokes_dir), 1, 1)
+            start=1,
 
-        ### marked checkbutton
+        ):
 
-        marked_checkbutton_strokes_dir = (
-            STROKES_DATA_DIR / 'marked_checkbutton_strokes_dir'
-        )
+            button = QPushButton("Set/reset strokes")
+            button.clicked.connect(
+                partial(self.reset_stroke, widget_key)
+            )
 
-        checked_checkbox = QCheckBox()
-        checked_checkbox.setCheckState(Qt.CheckState.Checked)
-
-        grid.addWidget(checked_checkbox, 2, 0)
-        grid.addWidget(StrokesDisplay(marked_checkbutton_strokes_dir), 2, 1)
+            grid.addWidget(get_widget(), row, 0)
+            grid.addWidget(StrokesDisplay(widget_key), row, 1)
+            grid.addWidget(button, row, 2)
 
         ###
         self.setLayout(self.grid)
 
+    def reset_stroke(self, widget_key):
+        print(widget_key)
+
 
 ### strokes display widget definition
+
+NOT_FOUND_SVG_BYTE_ARRAY = (
+
+    QByteArray(
+        get_not_found_icon_svg_text(
+            STROKE_SIZE.toTuple()
+        )
+    )
+
+)
 
 
 class StrokesDisplay(QWidget):
 
-    def __init__(self, strokes_dir):
+    def __init__(self, widget_key):
 
         super().__init__()
 
-        self.strokes_dir = strokes_dir
+        self.strokes_dir = strokes_dir = (
+            STROKES_DATA_DIR / f'{widget_key}_strokes_dir'
+        )
 
         if strokes_dir.exists():
 
-            npys = sorted(str(path) for path in strokes_dir.glob('*.npy'))
+            npys = (
+                sorted(
+                    str(path)
+                    for path in strokes_dir.glob('*.npy')
+                )
+            )
 
             if npys:
                 self.init_strokes_display(npys)
@@ -102,17 +151,7 @@ class StrokesDisplay(QWidget):
         layout = QHBoxLayout()
         svg_widget = QSvgWidget()
 
-        svg_widget.load(
-
-            QByteArray(
-
-                get_not_found_icon_svg_text(
-                    STROKE_SIZE.toTuple()
-                )
-
-            )
-
-        )
+        svg_widget.load((NOT_FOUND_SVG_BYTE_ARRAY))
 
         layout.addWidget(svg_widget)
         self.setLayout(layout)
