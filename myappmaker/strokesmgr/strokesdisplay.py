ef17c7532bb67390db1d7a06fd1e21548c50b6f7
Kennedy Richard <kennedy@kennedyrichard.com>
2025-02-22 16:59:49 -0300

    change: how strokes are shown in stroke settings
    That is, instead of showing strokes progressively laid one on top
    of the other in their original size, show their union in their
    original size and then, beside it, show smaller versions of the
    strokes progressively laid on top of each other.

--- myappmaker/strokesmgr/strokesdisplay.py
+++ myappmaker/strokesmgr/strokesdisplay.py
@@ -46,37 +46,40 @@ from .constants import (
 
 ### strokes display widget definition
 
-NOT_FOUND_SVG_BYTE_ARRAY = (
 
-    QByteArray(
-        get_not_found_icon_svg_text(STROKE_SIZE)
+SVG_RENDERER = (
+    QSvgRenderer(
+        QByteArray(
+            get_not_found_icon_svg_text(STROKE_SIZE)
+        )
     )
-
 )
-
-SVG_RENDERER = QSvgRenderer(NOT_FOUND_SVG_BYTE_ARRAY)
 
 TOP_STROKE_PEN = QPen()
 TOP_STROKE_PEN.setWidth(4)
 TOP_STROKE_PEN.setColor(Qt.black)
 
-BOTTOM_STROKE_PEN = QPen()
-BOTTOM_STROKE_PEN.setWidth(4)
-BOTTOM_STROKE_PEN.setColor(LIGHT_GREY_QCOLOR)
+THICKER_TOP_STROKE_PEN = QPen()
+THICKER_TOP_STROKE_PEN.setWidth(6)
+THICKER_TOP_STROKE_PEN.setColor(Qt.black)
+
+THICKER_BOTTOM_STROKE_PEN = QPen()
+THICKER_BOTTOM_STROKE_PEN.setWidth(6)
+THICKER_BOTTOM_STROKE_PEN.setColor(LIGHT_GREY_QCOLOR)
 
 START_POINT_BRUSH = QBrush()
 START_POINT_BRUSH.setColor(Qt.red)
 START_POINT_BRUSH.setStyle(Qt.SolidPattern)
 
 
-def _get_stroke_bg():
+def _get_stroke_bg(thickness=2):
 
     bg = QPixmap(*STROKE_SIZE)
     bg.fill(Qt.white)
     painter = QPainter(bg)
 
     pen = QPen()
-    pen.setWidth(2)
+    pen.setWidth(thickness)
     pen.setColor(LIGHT_GREY_QCOLOR)
     pen.setStyle(Qt.DashLine)
 
@@ -91,9 +94,15 @@ def _get_stroke_bg():
     painter.drawLine(*hline)
     painter.drawLine(*vline)
 
+    ###
+
     pen.setStyle(Qt.SolidLine)
+
+    half_thickness = thickness / 2
+
     painter.setPen(pen)
-    painter.drawLine(width-1, 0, width-1, height)
+    painter.drawLine(width-half_thickness, 0, width-half_thickness, height)
+    painter.drawLine(0, height-half_thickness, width, height-half_thickness)
     painter.end()
 
     return bg
@@ -110,7 +119,9 @@ class StrokesDisplay(QWidget):
         ###
 
         if self.__class__.stroke_bg is None:
-            self.__class__.stroke_bg = _get_stroke_bg()
+
+            self.__class__.stroke_bg = _get_stroke_bg(2)
+            self.__class__.thicker_stroke_bg = _get_stroke_bg(4)
 
         ###
 
@@ -146,7 +157,7 @@ class StrokesDisplay(QWidget):
         pixmap = QPixmap(*STROKE_SIZE)
         pixmap.fill(Qt.transparent)
         painter = QPainter(pixmap)
-        SVG_RENDERER.render(painter)#, pixmap.rect())
+        SVG_RENDERER.render(painter)
         painter.end()
         self.label.setPixmap(pixmap)
 
@@ -191,72 +202,167 @@ class StrokesDisplay(QWidget):
 
     def get_new_pixmap(self, strokes):
 
-        n = len(strokes)
-        width = n * STROKE_DIMENSION
-        height = STROKE_DIMENSION
+        full_size_strokes_pm = (
+            get_full_size_strokes_pixmap(strokes, self.stroke_bg)
+        )
+
+        half_size_strokes_pm = (
+            get_half_size_strokes_pixmap(strokes, self.thicker_stroke_bg)
+        )
+
+        fw, fh = full_size_strokes_pm.size().toTuple()
+        hw, hh = half_size_strokes_pm.size().toTuple()
+
+        width = fw + hw
+        height = max(fh, hh)
 
         pixmap = QPixmap(width, height)
-
+        pixmap.fill(Qt.transparent)
         painter = QPainter(pixmap)
-        painter.drawTiledPixmap(0, 0, width, height, self.stroke_bg)
 
-        x_increment = -STROKE_HALF_DIMENSION
-        y_increment = 150
-
-        strokes_deque = deque(strokes)
-        bottom_strokes = []
-
-        while strokes_deque:
-
-            top_stroke = strokes_deque.popleft()
-
-            x_increment += STROKE_DIMENSION
-
-            for points, pen, point_on_start in (
-
-                *zip(
-                    bottom_strokes,
-                    repeat(BOTTOM_STROKE_PEN),
-                    repeat(False),
-                ),
-                (top_stroke, TOP_STROKE_PEN, True),
-
-            ):
-
-                offset_points = [
-
-                    QPointF(
-                        a+x_increment,
-                        b+y_increment,
-                    )
-
-                    for a, b in points
-
-                ]
-
-                painter.setPen(pen)
-                painter.drawPolyline(offset_points)
-
-                if point_on_start:
-
-                    ###
-
-                    painter.setPen(Qt.NoPen)
-                    painter.setBrush(START_POINT_BRUSH)
-                    painter.setOpacity(.6)
-
-                    ###
-                    painter.drawEllipse(QPointF(offset_points[0]), 8, 8)
-
-                    ###
-
-                    painter.setPen(Qt.SolidLine)
-                    painter.setBrush(Qt.NoBrush)
-                    painter.setOpacity(1.0)
-
-            bottom_strokes.append(top_stroke)
-
+        painter.drawPixmap(0, 0, full_size_strokes_pm)
+        painter.drawPixmap(STROKE_DIMENSION, 0, half_size_strokes_pm)
         painter.end()
 
         return pixmap
+
+
+def get_full_size_strokes_pixmap(strokes, stroke_bg):
+
+    pixmap = QPixmap(STROKE_DIMENSION, STROKE_DIMENSION)
+    pixmap.fill(Qt.white)
+    painter = QPainter(pixmap)
+    painter.drawPixmap(0, 0, stroke_bg)
+
+    painter.setPen(TOP_STROKE_PEN)
+
+    offset = STROKE_HALF_DIMENSION
+
+    for points in strokes:
+
+        painter.drawPolyline(
+
+            [
+
+                QPointF(
+                    a+offset,
+                    b+offset,
+                )
+
+                for a, b in points
+
+            ]
+
+        )
+
+    painter.end()
+
+    return pixmap
+
+
+def get_half_size_strokes_pixmap(strokes, stroke_bg):
+
+    ### small strokes
+
+    no_of_strokes = len(strokes)
+    no_of_cols = 5
+    no_of_rows, remainder = divmod(no_of_strokes, no_of_cols)
+
+    if remainder:
+        no_of_rows += 1
+
+    ###
+
+    width = (
+
+        no_of_cols
+        if no_of_strokes >= 6
+
+        else no_of_strokes
+
+    ) * STROKE_DIMENSION
+
+    height = no_of_rows * STROKE_DIMENSION
+
+    pixmap = QPixmap(width, height)
+    pixmap.fill(Qt.transparent)
+    painter = QPainter(pixmap)
+
+    ###
+
+    strokes_deque = deque(strokes)
+    bottom_strokes = []
+
+    row = 0
+    col = 0
+
+    while strokes_deque:
+
+        if col == no_of_cols:
+
+            col = 0
+            row += 1
+
+        left = col * STROKE_DIMENSION
+        top = row * STROKE_DIMENSION
+
+        painter.drawPixmap(left, top, stroke_bg)
+
+        top_stroke = strokes_deque.popleft()
+
+        x_offset = (col * STROKE_DIMENSION) + STROKE_HALF_DIMENSION
+        y_offset = (row * STROKE_DIMENSION) + STROKE_HALF_DIMENSION
+
+        for points, pen, point_on_start in (
+
+            *zip(
+                bottom_strokes,
+                repeat(THICKER_BOTTOM_STROKE_PEN),
+                repeat(False),
+            ),
+            (top_stroke, THICKER_TOP_STROKE_PEN, True),
+
+        ):
+
+            offset_points = [
+
+                QPointF(
+                    a+x_offset,
+                    b+y_offset,
+                )
+
+                for a, b in points
+
+            ]
+
+            painter.setPen(pen)
+            painter.drawPolyline(offset_points)
+
+            if point_on_start:
+
+                ###
+
+                painter.setPen(Qt.NoPen)
+                painter.setBrush(START_POINT_BRUSH)
+                painter.setOpacity(.6)
+
+                ###
+                painter.drawEllipse(QPointF(offset_points[0]), 8, 8)
+
+                ###
+
+                painter.setPen(Qt.SolidLine)
+                painter.setBrush(Qt.NoBrush)
+                painter.setOpacity(1.0)
+
+        bottom_strokes.append(top_stroke)
+
+        col += 1
+
+    painter.end()
+
+    tmode = Qt.TransformationMode.SmoothTransformation
+    pixmap = pixmap.scaledToWidth(width/2, tmode)
+
+    return pixmap
 
