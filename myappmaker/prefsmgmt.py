085c1d531d433d0cd817dea2a66a6692242e6bfb
Kennedy Richard <kennedy@kennedyrichard.com>
2025-04-26 16:09:57 -0300

    temp: add more preferences

--- myappmaker/prefsmgmt.py
+++ myappmaker/prefsmgmt.py
@@ -31,15 +31,25 @@ from .ourstdlibs.pyl import load_pyl, save_pyl
 
 
 
+### TODO add label and controls for the new preferences
+### (ratio log diff tolerance and max tolerable Hausdorff distance)
+
+
+
 ### module-level objects/constants
 
 @unique
 class PreferencesKeys(Enum):
     SHOW_WIDGET_MENU_AFTER_DRAWING = 'show_widget_menu_after_drawing'
-
+    RATIO_LOG_DIFF_TOLERANCE = 'ratio_log_diff_tolerance'
+    MAXIMUM_TOLERABLE_HAUSDORFF_DISTANCE = (
+        'maximum_tolerable_hausdorff_distance'
+    )
 
 DEFAULT_PREFERENCES = {
     PreferencesKeys.SHOW_WIDGET_MENU_AFTER_DRAWING.value: True,
+    PreferencesKeys.RATIO_LOG_DIFF_TOLERANCE.value: 0.6,
+    PreferencesKeys.MAXIMUM_TOLERABLE_HAUSDORFF_DISTANCE.value: 60,
 }
 
 PREFERENCES = DEFAULT_PREFERENCES.copy()
@@ -47,40 +57,6 @@ PREFERENCES = DEFAULT_PREFERENCES.copy()
 BOLD_TEXT_CSS = 'font-weight: bold;'
 
 
-### validation function
-
-def validate_preferences(preferences):
-
-    key = PreferencesKeys.SHOW_WIDGET_MENU_AFTER_DRAWING.value
-
-    if key not in preferences:
-        raise KeyError(f"Didn't found {key!r} in preferences")
-
-    elif type(preferences[key]) != bool:
-        raise TypeError(f"value of {key!r} key in preferences must be a boolean")
-
-### utility function
-
-def get_button_like_label(text):
-
-    btn = QPushButton(text)
-
-    btn.setStyleSheet("""
-    QPushButton {
-        border: none;
-        background: transparent;
-        text-align: left;
-        color: palette(window-text);
-    }
-    QToolTip {
-        background-color: #333;
-        color: #fff;
-        border: 1px solid #ccc;
-        padding: 2px;
-    }
-    """)
-
-    return btn
 
 ### dialog definition
 
@@ -95,43 +71,8 @@ class PreferencesDialog(QDialog):
         ###
         self.widget_map = {}
 
-        ### if the preferences file doesn't exist, create it
-
-        if (
-            not PREFERENCES_FILEPATH.is_file()
-            and not PREFERENCES_FILEPATH.exists()
-        ):
-
-            try: save_pyl(PREFERENCES, PREFERENCES_FILEPATH)
-            except Exception as err:
-                print(f"Failed to create preferences file: {err}")
-
-        ### load preferences
-
-        else:
-
-            try: prefs = load_pyl(PREFERENCES_FILEPATH)
-
-            except Exception as err:
-
-                print(
-                    "Preferences couldn't be loaded (using defaults instead):"
-                    f" {err}"
-                )
-
-            else:
-
-                try: validate_preferences(prefs)
-
-                except Exception as err:
-
-                    print(
-                        "Loaded preferences didn't validate"
-                        f" (using defaults instead): {err}"
-                    )
-
-                else:
-                    PREFERENCES.update(prefs)
+        ###
+        prepare_preferences()
 
         ###
 
@@ -209,8 +150,9 @@ class PreferencesDialog(QDialog):
                 "Checkbox shouldn't have a state other than Checked/Unchecked"
             )
 
-        key = PreferencesKeys.SHOW_WIDGET_MENU_AFTER_DRAWING.value
-        PREFERENCES[key] = value
+        PREFERENCES[
+            PreferencesKeys.SHOW_WIDGET_MENU_AFTER_DRAWING.value
+        ] = value
 
         try: save_pyl(PREFERENCES, PREFERENCES_FILEPATH)
         except Exception as err:
@@ -226,3 +168,83 @@ class PreferencesDialog(QDialog):
                 'Checked' if not widget.isChecked() else 'Unchecked',
             )
         )
+
+
+### helper functions
+
+def validate_preferences(preferences):
+
+    for key, default_value in DEFAULT_PREFERENCES.items():
+
+        value_type = type(default_value)
+
+        if (
+            key in preferences
+            and value_type != type(preferences[key])
+        ):
+
+            raise TypeError(
+                f"If the '{key!r}' key is present in preferences,"
+                f" it must of {value_type} type"
+            )
+
+def get_button_like_label(text):
+
+    btn = QPushButton(text)
+
+    btn.setStyleSheet("""
+    QPushButton {
+        border: none;
+        background: transparent;
+        text-align: left;
+        color: palette(window-text);
+    }
+    QToolTip {
+        background-color: #333;
+        color: #fff;
+        border: 1px solid #ccc;
+        padding: 2px;
+    }
+    """)
+
+    return btn
+
+def prepare_preferences():
+
+    ### if the preferences file doesn't exist, create it
+
+    if (
+        not PREFERENCES_FILEPATH.is_file()
+        and not PREFERENCES_FILEPATH.exists()
+    ):
+
+        try: save_pyl(PREFERENCES, PREFERENCES_FILEPATH)
+        except Exception as err:
+            print(f"Failed to create preferences file: {err}")
+
+    ### load preferences
+
+    else:
+
+        try: prefs = load_pyl(PREFERENCES_FILEPATH)
+
+        except Exception as err:
+
+            print(
+                "Preferences couldn't be loaded (using defaults instead):"
+                f" {err}"
+            )
+
+        else:
+
+            try: validate_preferences(prefs)
+
+            except Exception as err:
+
+                print(
+                    "Loaded preferences didn't validate"
+                    f" (using defaults instead): {err}"
+                )
+
+            else:
+                PREFERENCES.update(prefs)
