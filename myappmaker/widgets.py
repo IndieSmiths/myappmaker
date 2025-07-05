85115c128659aedf43cd078a94fe9713424c3301
Kennedy Richard <kennedy@kennedyrichard.com>
2025-07-05 10:33:34 -0300

    throwaway: testing custom font

--- myappmaker/widgets.py
+++ myappmaker/widgets.py
@@ -25,7 +25,13 @@ from PySide6.QtWidgets import (
 
 from PySide6.QtCore import Qt, QPointF, QLineF, QRectF, QMarginsF
 
-from PySide6.QtGui import QPainterPath, QPen, QColorConstants
+from PySide6.QtGui import (
+    QPainterPath,
+    QPen,
+    QColorConstants,
+    QFontDatabase,
+    QFont,
+)
 
 
 
@@ -61,8 +67,21 @@ class LabelItem(QGraphicsSimpleTextItem):
 
     def __init__(self):
 
+
         super().__init__()
-        super().setText('A label')
+        super().setText('This is a label')
+
+        if not hasattr(self, 'my_font'):
+
+            font_id = QFontDatabase.addApplicationFont(
+                'caveat_variable_font_wght.ttf'
+            )
+
+            families = QFontDatabase.applicationFontFamilies(font_id)
+            self.my_font = QFont(families, 32)
+
+        super().setFont(self.my_font)
+
 
 
 random_offset = (
